b90cbb24688d5d393007bb06c3343f0229633b46
ingmarsretro <118366008+ingmarsretro@users.noreply.github.com>
2023-03-01 16:06:26 +0100

    Add files via upload

---
+++ vendor/phpgt/dom/test/phpunit/HTMLDocumentTest.php
@@ -0,0 +1,45 @@
+<?php
+namespace Gt\Dom\Test;
+
+use Error;
+use Gt\Dom\Facade\HTMLDocumentFactory;
+use PHPUnit\Framework\TestCase;
+use Gt\Dom\HTMLDocument;
+use Throwable;
+
+class HTMLDocumentTest extends TestCase {
+	public function testCanConstruct():void {
+		$exception = null;
+
+		try {
+			new HTMLDocument();
+		}
+		catch(Throwable $exception) {}
+
+		self::assertNull($exception);
+	}
+
+	public function testCreatesRootNode():void {
+		$sut = HTMLDocumentFactory::create("<h1>Test</h1>");
+		self::assertEquals(
+			"HTML",
+			$sut->documentElement->tagName
+		);
+	}
+
+	public function testCreatedElementsAreNotNamespaced():void {
+		$sut = HTMLDocumentFactory::create('<html lang="en"><head><title>Test</title></head><body></body></html>');
+		$div = $sut->createElement("div");
+		$sut->body->appendChild($div);
+		self::assertEquals(
+			"<div></div>",
+			$sut->body->innerHTML
+		);
+	}
+
+	public function testEmojiEncoding():void {
+		$html = "<h1>I ❤️ my 🐈</h1>";
+		$sut = HTMLDocumentFactory::create($html);
+		self::assertStringContainsString("$html", (string)$sut);
+	}
+}
